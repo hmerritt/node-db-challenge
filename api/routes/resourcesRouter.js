e9f1a504c0fe86f017df1ef9c0cec7fe77eb672b
Harry Merritt <harry@merritt.es>
2020-05-04 02:21:56 +0100

    PUT, DELETE: resource

--- api/routes/resourcesRouter.js
+++ api/routes/resourcesRouter.js
@@ -23,7 +23,7 @@ router.get("/:id", db.validateId(table), async (req, res, next) => {
     res.send(req.record);
 });
 
-//  Create a new resources
+//  Create a new resource
 router.post("/", db.validateBody(table, validateBodyCb), async (req, res, next) => {
     try {
         const [record_id] = await db.insert(table, req.recordBody);
@@ -31,6 +31,39 @@ router.post("/", db.validateBody(table, validateBodyCb), async (req, res, next) 
             id: record_id,
             ...req.recordBody
         });
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+//  Update existing resource
+router.put("/:id", db.validateBody(table, validateBodyCb), db.validateId(table), async (req, res, next) => {
+    try {
+        const record = await db.update(table, req.record.id, req.recordBody);
+        res.json({
+            id: req.record.id,
+            ...req.recordBody
+        });
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+//  Delete resource
+router.delete("/:id", db.validateId(table), async (req, res, next) => {
+    try {
+        const record = await db.remove(table, req.record.id);
+        if (record) {
+            res.json({
+                message: `${table} {${req.record.id}} deleted successfully`
+            });
+        }
+        else
+        {
+            next(err);
+        }
     }
     catch (err) {
         next(err);
