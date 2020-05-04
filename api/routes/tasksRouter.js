5ae395813c6c95d177ecee5ad63541b6e0f74c15
Harry Merritt <harry@merritt.es>
2020-05-04 02:41:32 +0100

    All CURD operations for tasks endpoint

---
+++ api/routes/tasksRouter.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const db = require("./db");
+
+const router = express.Router();
+const table = "tasks";
+const validateBodyCb = (req) => {
+    if (req.body.project_id && req.body.description) {
+        req.recordBody = {
+            project_id: req.body.project_id,
+            description: req.body.description,
+            notes: req.body.notes,
+            completed: (req.body.completed || false)
+        }
+    }
+    return req;
+}
+
+//  Get all tasks
+router.get("/", db.validateId(table), async (req, res, next) => {
+    res.send(req.record);
+});
+
+//  Get individial
+router.get("/:id", db.validateId(table), async (req, res, next) => {
+    res.send(req.record);
+});
+
+//  Get individial tasks
+router.get("/:id/tasks", db.validateId(table), async (req, res, next) => {
+    try {
+        const record = await db.getTasks(req.record.id);
+        res.json(record);
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+//  Create a new task
+router.post("/", db.validateBody(table, validateBodyCb), async (req, res, next) => {
+    try {
+        const [record_id] = await db.insert(table, req.recordBody);
+        res.json({
+            id: record_id,
+            ...req.recordBody
+        });
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+//  Update existing task
+router.put("/:id", db.validateBody(table, validateBodyCb), db.validateId(table), async (req, res, next) => {
+    try {
+        const record = await db.update(table, req.record.id, req.recordBody);
+        res.json({
+            id: req.record.id,
+            ...req.recordBody
+        });
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+//  Delete task
+router.delete("/:id", db.validateId(table), async (req, res, next) => {
+    try {
+        const record = await db.remove(table, req.record.id);
+        if (record) {
+            res.json({
+                message: `${table} {${req.record.id}} deleted successfully`
+            });
+        }
+        else
+        {
+            next(err);
+        }
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const projectsRouter = require("./routes/projectsRouter");
 const resourcesRouter = require("./routes/resourcesRouter");
+const tasksRouter = require("./routes/tasksRouter");
 
 const server = express();
 
 server.use(express.json());
 server.use("/projects", projectsRouter);
 server.use("/resources", resourcesRouter);
+server.use("/tasks", tasksRouter);
 
 module.exports = server;
